9563b119927fc5750d653328ed71a24b08354325
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2025-01-27 12:24:32 +0000

    basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Hello</Button>;
+  return (
+    <Grid templateAreas={`"nav nav" "aside main"`}>
+      <GridItem area="nav" bg="coral">
+        Navbar
+      </GridItem>
+      <GridItem area="aside" bg="brown">
+        SIDE
+      </GridItem>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
