79ee26e2666d02ceaadf4d0df843065a0dc33de8
motunrayo-o <motunrayo.ogunyinka@gmail.com>
2025-01-27 12:46:40 +0000

    Basic NavBar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
         lg: `"nav nav" "aside main"`, //1024px width
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Navbar
+      <GridItem area="nav">
+        <NavBar></NavBar>
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="brown">
